ee10b57fa5a3f9d7207bdd21dec2a92c61c0665e
EliScherrer <escherre@purdue.edu>
2018-02-11 15:33:56 -0500

    added autolayout

--- twitter_alamofire_demo/TweetCell.swift
+++ twitter_alamofire_demo/TweetCell.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class TweetCell: UITableViewCell {
     
     @IBOutlet weak var tweetTextLabel: UILabel!
+    @IBOutlet weak var timestampLabel: UILabel!
+    
+    @IBOutlet weak var profilePicture: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var userHandleLabel: UILabel!
+    
+    @IBOutlet weak var retweetImage: UIImageView!
+    @IBOutlet weak var retweetCountLabel: UILabel!
+    
+    @IBOutlet weak var favoriteImage: UIImageView!
+    @IBOutlet weak var favoriteCountLabel: UILabel!
     
     var tweet: Tweet! {
         didSet {
             tweetTextLabel.text = tweet.text
+            timestampLabel.text = tweet.createdAtString
+            
+            retweetCountLabel.text = "\(tweet.retweetCount)"
+            if tweet.retweeted == true {
+                retweetImage.image = #imageLiteral(resourceName: "retweet-icon-green")
+            }
+            else {
+                retweetImage.image = #imageLiteral(resourceName: "retweet-icon")
+            }
+            
+            favoriteCountLabel.text = "\(tweet.favoriteCount ?? 0)"
+            if tweet.favorited == true {
+                favoriteImage.image = #imageLiteral(resourceName: "favor-icon-red")
+            }
+            else {
+                favoriteImage.image = #imageLiteral(resourceName: "favor-icon")
+            }
+            
+            userNameLabel.text = tweet.user.name
+            userHandleLabel.text = tweet.user.screenname
+            profilePicture.af_setImage(withURL: tweet.user.profileUrl!)
         }
     }
     
