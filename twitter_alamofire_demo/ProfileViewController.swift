9ccbb9f35c880bbe150771fe38bfbf5f0829a373
EliScherrer <escherre@purdue.edu>
2018-02-17 20:28:43 -0500

    added profile and compose views

---
+++ twitter_alamofire_demo/ProfileViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileViewController.swift
+//  twitter_alamofire_demo
+//
+//  Created by Eli Scherrer on 2/17/18.
+//  Copyright © 2018 Charles Hieger. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
